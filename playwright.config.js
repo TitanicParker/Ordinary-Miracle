ebc43796c973774acf6f07096b4ee0d25977bfa5
TitanicParker <theliamo@gmail.com>
2026-07-28 15:26:45 +0100

    Configure phone-sized browser tests

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  use: {
+    baseURL: 'http://127.0.0.1:4173',
+    trace: 'retain-on-failure'
+  },
+  webServer: {
+    command: 'python3 -m http.server 4173 --bind 127.0.0.1',
+    port: 4173,
+    reuseExistingServer: false
+  },
+  projects: [
+    {
+      name: 'mobile-chromium',
+      use: { ...devices['iPhone 13'] }
+    }
+  ]
+});
